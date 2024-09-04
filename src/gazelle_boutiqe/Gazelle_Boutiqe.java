d6ccf4aacfbfad79113ef691c11e84fbc0cb9dea
Zaki Tahiry <tahiryzaki@gmail.com>
2024-09-04 01:11:17 -0700

    initial commit

---
+++ src/gazelle_boutiqe/Gazelle_Boutiqe.java
@@ -0,0 +1,13 @@
+
+package gazelle_boutiqe;
+
+
+public class Gazelle_Boutiqe {
+    
+
+    public static void main(String[] args) {
+      home obj=new home();
+      obj.setVisible(true);
+    }
+    
+}
